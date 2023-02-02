7639b0621d872de062226289af018b5ed1a5d4e4
Azxki <91349029+Azxki@users.noreply.github.com>
2023-02-02 17:51:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,104 @@
 <?php
 
-/**
- * Pour cet exercice, vous allez utiliser la base de données table_test_php créée pendant l'exo 189
- * Vous utiliserez également les deux tables que vous aviez créées au point 2 ( créer des tables avec PHP )
- */
+    $username = 'root';
+    $password = '';
+
 
 try {
-    /**
-     * Créez ici votre objet de connection PDO, et utilisez à chaque fois le même objet $pdo ici.
-     */
-    $pdo = new P...;
+    $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     /**
      * 1. Insérez un nouvel utilisateur dans la table utilisateur.
      */
+    $dt = new DateTime();
+    $date = $dt->format('Y-m-d');
 
-    // TODO votre code ici.
+    $sql = `
+        INSERT INTO utilisateurs (id, nom, prenom, email, password, adresse, code_postal, pays, $date)
+        VALUES ('Doe', 'John', 'Arlette Corrente', 5, 59610, 'France', 'j.doe@gondation.org', '$date')
+        `;
 
 
     /**
      * 2. Insérez un nouveau produit dans la table produit
      */
 
-    // TODO votre code ici.
+    $sql2 = `
+        INSERT INTO utilisateurs (id, titre, prix, description_courte, description_longue)
+        VALUES ('1', 'Article 1', '60,80', voiture, voiture de 2006 lorem lorem lorem')
+        `;
 
     /**
      * 3. En une seule requête, ajoutez deux nouveaux utilisateurs à la table utilisateur.
      */
 
-    // TODO Votre code ici.
+    $result = $db->exec($sql & $sql);
 
     /**
      * 4. En une seule requête, ajoutez deux produits à la table produit.
      */
 
-    // TODO Votre code ici.
+    $result2 = $db->exec($sql2 & $sql2);
+    }
+        catch (PDOException $exception) {
+        echo $exception->getMessage();
+    }
 
     /**
      * 5. A l'aide des méthodes beginTransaction, commit et rollBack, insérez trois nouveaux utilisateurs dans la table utilisateur.
      */
 
-    // TODO Votre code ici.
+    try {
+        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', $username, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+        $db->beginTransaction();
+
+        $insert = `
+        INSERT INTO utilisateurs (id, nom, prenom, email, password, adresse, code_postal, pays, $date)
+        `;
+
+        $sql3 = $insert . "('Doe', 'John', 'Arlette Corrente', 5, 59610, 'France', 'j.doe@gondation.org', '$date')";
+        $db->exec($sql3);
+        $sql4 = $insert . "('Doe', 'John', 'Arlette Corrente', 5, 59610, 'France', 'j.doe@gondation.org', '$date')";
+        $db->exec($sql4);
+        $sql5 = $insert . "('Doe', 'John', 'Arlette Corrente', 5, 59610, 'France', 'j.doe@gondation.org', '$date')";
+        $db->exec($sql5);
+
+        $db->commit();
+    }
+    catch (PDOException $exception) {
+        echo $exception->getMessage();
+
+        $db->rollBack();
+    }
 
     /**
      * 6. A l'aide des méthodes beginTransaction, commit et rollBack, insérez trois nouveaux produits dans la table produit.
      */
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $db->beginTransaction();
+
+    $insert = `
+        INSERT INTO utilisateurs (id, titre, prix, description_courte, description_longue)
+        `;
+
+    $sql6 = $insert . "('1', 'Article 1', '60,80', voiture, voiture de 2006 lorem lorem lorem')";
+    $db->exec($sql6);
+    $sql7 = $insert . "('1', 'Article 1', '60,80', voiture, voiture de 2006 lorem lorem lorem')";
+    $db->exec($sql7);
+    $sql8 = $insert . "('1', 'Article 1', '60,80', voiture, voiture de 2006 lorem lorem lorem')";
+    $db->exec($sql8);
+
+    $db->commit();
 }
-catch (...)
+catch (PDOException $exception) {
+    echo $exception->getMessage();
+
+    $db->rollBack();
+}
